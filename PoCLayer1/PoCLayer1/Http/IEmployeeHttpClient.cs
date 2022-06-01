621496ca796d9c88e984745bf39c33c218ec40e7
Alex <devteam.aleks@gmail.com>
2022-06-01 12:37:10 +0200

    Users - Admin and Employee and now edit their own accounts.

--- PoCLayer1/PoCLayer1/Http/IEmployeeHttpClient.cs
+++ PoCLayer1/PoCLayer1/Http/IEmployeeHttpClient.cs
@@ -8,8 +8,6 @@ public interface IEmployeeHttpClient
     public Task<Employee> GetEmployeeByIdAsync(long id);
     public Task<ICollection<Employee>> GetAllEmployeesAsync();
     public Task<Employee> DeleteEmployeeByIdAsync(long id);
-    public Task<Employee> UpdateEmployeeAsync(Employee employee);
-    public Task<string> CheckEmployeeAuthState(Employee employee);
-    public Task<bool> CheckIfEmployeeExists(Employee employee);
-    public Task<Employee> GetEmployee(Employee employee);
+    public Task<Employee> UpdateEmployeeAsync(long id, Employee employee);
+
 }
